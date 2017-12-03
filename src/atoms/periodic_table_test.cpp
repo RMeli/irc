ffc9ca5f308b078803091bccd05ae2a45ac50128
Rocco Meli <r.meli@bluemail.ch>
2017-12-03 17:06:45 +0000

    Start migration to catch

--- src/atoms/periodic_table_test.cpp
+++ src/atoms/periodic_table_test.cpp
@@ -1,37 +1,47 @@
-#undef NDEBUG
+#define CATCH_CONFIG_MAIN
+#include "../catch/catch.hpp"
 
 #include "periodic_table.h"
 
-#include <cassert>
-#include <iomanip>
 #include <iostream>
 
-int main(){
-  using namespace std;
-  using namespace periodic_table;
+using namespace periodic_table;
+using namespace std;
+
+TEST_CASE("Test periodic table","[periodic_table]"){
   
-  bool valid{false};
-  for(size_t i{0}; i < pt_size + 1; i++){
-    valid = valid_atomic_number(i);
+  // Check periodic table size for supported elements
+  SECTION("size"){
+    REQUIRE( pt_size == 96 );
+  }
+  
+  // Check valid atomic numbers
+  SECTION("atomic numbers"){
+    bool valid{false};
+    for(size_t i{0}; i < pt_size + 1; i++){
+      valid = valid_atomic_number(i);
     
-    if(valid){
-      cout << std::setw(3) << i;
-      cout << " is a valid atomic number corresponding to element ";
-      cout << pt_symbols[i] << ".\n";
+      if(valid){
+        cout << std::setw(3) << i;
+        cout << " is a valid atomic number corresponding to element ";
+        cout << pt_symbols[i] << ".\n";
       
-      assert( i < pt_size );
-    }
-    else{
-      cout << std::setw(3) << i;
-      cout << " is not a valid atomic number.\n";
+        REQUIRE( i < pt_size);
+      }
+      else{
+        cout << std::setw(3) << i;
+        cout << " is not a valid atomic number.\n";
       
-      assert( i == 0 or i == pt_size );
+        REQUIRE( (i == 0 or i == pt_size) );
+      }
     }
   }
   
-  for(size_t i{0}; i < pt_size; i++){
-    assert( atomic_number(pt_symbols[i]) == i );
+  // Check conversion from symbol to atomic number
+  SECTION("atomic number from symbol"){
+    for(size_t i{0}; i < pt_size; i++){
+      REQUIRE( atomic_number(pt_symbols[i]) == i );
+    }
   }
   
-  return 0;
 }
