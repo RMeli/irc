d77379a2ca3f111e432daf1dfc2e2ae43497ad18
Rocco Meli <r.meli@bluemail.ch>
2018-01-08 17:29:28 +0000

    irc_to_cartesian test with dihedral angles

--- src/transformation/transformation_test.cpp
+++ src/transformation/transformation_test.cpp
@@ -321,4 +321,154 @@ TEST_CASE("Transformation"){
       REQUIRE( angle(p1,p2,p3) == target);
     }
   }
+
+  SECTION("Internal to cartesian for H2O2") {
+    using namespace molecule;
+    using namespace connectivity;
+    using namespace transformation;
+    using namespace tools::conversion;
+    using namespace io;
+    using namespace std;
+    using namespace wilson;
+
+    // Load molecule from file
+    Molecule<vec3> molecule{load_xyz<vec3>("../../test/hydrogen_peroxide.xyz")};
+
+    // Transform molecular coordinates from angstrom to bohr
+    multiply_positions(molecule, angstrom_to_bohr);
+
+    // Compute interatomic distance for formaldehyde molecule
+    mat dd{ distances<vec3, mat>(molecule) };
+
+    // Build graph based on the adjacency matrix
+    UGraph adj{ adjacency_matrix(dd, molecule) };
+
+    // Compute distance matrix and predecessor matrix
+    mat dist, predecessors;
+    std::tie(dist, predecessors) = distance_matrix<mat>(adj);
+
+    // Compute bonds
+    std::vector<Bond> B{ bonds(dist, molecule) };
+
+    // Print bonds
+    if(verbose){
+      cout << '\n' << B.size() << " bonds (a.u.):" << endl;
+      for(const auto& b : B){
+        cout << '(' << b.i + 1 << ',' << b.j + 1 << ") "
+             << connectivity::bond(b, molecule) << endl;
+      }
+    }
+
+    // Check number of bonds
+    REQUIRE( B.size() == 3);
+
+    // Compute angles
+    std::vector<Angle> A{ angles(dist, predecessors, molecule) };
+
+    // Print angles
+    if(verbose){
+      cout << '\n' << A.size() << " angles (deg):" << endl;
+      for(const auto& a : A){
+        cout << '(' << a.i + 1 << ',' << a.j + 1 << ',' << a.k + 1 << ") "
+             << connectivity::angle(a, molecule) << endl;
+      }
+    }
+
+    // Check number of angles
+    REQUIRE( A.size() == 2);
+
+    // Compute dihedral angles
+    std::vector<Dihedral> D{ dihedrals(dist, predecessors, molecule) };
+
+    // Print dihedrals
+    if(verbose){
+      cout << '\n' << D.size() << " dihedrals (deg):" << endl;
+      for(const auto& d : D){
+        cout << '(' << d.i + 1 << ',' << d.j + 1
+             << ',' << d.k + 1 << ',' << d.l + 1 << ") "
+             << connectivity::dihedral(d, molecule) << endl;
+      }
+    }
+
+    // Check number of dihedrals
+    REQUIRE( D.size() == 1);
+
+    // Compute Wilson B matrix
+    mat W = wilson_matrix<vec3,vec,mat>(
+        molecule::to_cartesian<vec3,vec>(molecule), B, A, D);
+
+    // Compute G matrices
+    mat G, iG;
+    std::tie(G, iG) = G_matrices(W);
+
+    // Allocate vector for internal reaction coordinates
+    vec q_irc_old{
+        irc_from_bad<vec>(
+            molecule::to_cartesian<vec3,vec>(molecule), B, A, D
+        )
+    };
+
+    // Displacement in internal coordinates
+    vec dq_irc{ 0.0, 0.0, 0.0, 0.0, 0.0, 0.1 };
+
+    // Compute new internal coordinates
+    vec q_irc_new{ q_irc_old + dq_irc };
+
+    // Print new internal coordinates
+    if(verbose){
+      cout << "\nNew internal coordinates:\n " << q_irc_new << endl;
+    }
+
+    // Compute number of cartesian coordinates
+    size_t n_c{ 3 * molecule.size() };
+
+    // Allocate vector for cartesian positions
+    vec x_c_old{ to_cartesian<vec3,vec>(molecule) };
+
+    // Compute new cartesian coordinates
+    vec x_c{
+        irc_to_cartesian<vec3,vec,mat>(q_irc_old, dq_irc, x_c_old, B, A, D)
+    };
+
+    // Print cartesian coordinates
+    if(verbose){
+      cout << "\nNew cartesian coordinates (a.u.):\n " << x_c << endl;
+    }
+
+    // Reconstruct atomic positions (points in 3D space)
+    vec3 p1{x_c(0), x_c(1), x_c(2)};
+    vec3 p2{x_c(3), x_c(4), x_c(5)};
+    vec3 p3{x_c(6), x_c(7), x_c(8)};
+    vec3 p4{x_c(9), x_c(10), x_c(11)};
+
+    SECTION("Bond 1"){
+      Approx target{q_irc_new(0)};
+      target.margin(1e-6);
+      REQUIRE( distance(p1,p2) == target);
+    }
+
+    SECTION("Bond 2"){
+      Approx target{q_irc_new(1)};
+      target.margin(1e-6);
+      REQUIRE( distance(p1,p3) == target);
+    }
+
+    SECTION("Bond 3"){
+      Approx target{q_irc_new(2)};
+      target.margin(1e-6);
+      REQUIRE( distance(p2,p4) == target);
+    }
+
+    SECTION("Angle 1"){
+      Approx target{q_irc_new(3)};
+      target.margin(1e-6);
+      REQUIRE( angle(p2,p1,p3) == target);
+    }
+
+    SECTION("Angle 2"){
+      Approx target{q_irc_new(4)};
+      target.margin(1e-6);
+      REQUIRE( angle(p1,p2,p4) == target);
+    }
+  }
 }
