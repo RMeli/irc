f8accfbf832cb2540f5ada8bf7a22a4d5a83749d
Rocco Meli <r.meli@bluemail.ch>
2018-01-15 14:29:49 +0000

    More doc in linalg.h

--- include/libirc/conversion.h
+++ include/libirc/conversion.h
@@ -7,7 +7,10 @@ namespace tools {
 
 namespace conversion {
 
+/// Angstrom to bohr conversion factor
 constexpr double angstrom_to_bohr{1.889725989};
+
+/// Bohr to angstrom conversion factor
 constexpr double bohr_to_angstrom{1. / angstrom_to_bohr};
 
 } // namespace irc
