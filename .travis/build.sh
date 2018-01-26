1652eb8014d88378cd2a2dd2b0b2b8154bc8678d
Rocco Meli <r.meli@bluemail.ch>
2018-01-26 13:24:21 +0000

    Corrected path
    Bug in build script

--- .travis/build.sh
+++ .travis/build.sh
@@ -6,7 +6,7 @@ cd build
 
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
     cmake -DCMAKE_C_COMPILER=$CC -DCMAKE_CXX_COMPILER=$CXX -DWITH_ARMA:BOOLEAN=TRUE ..
-elif [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
+#elif [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
     #cmake -DCMAKE_C_COMPILER=$CC -DCMAKE_CXX_COMPILER=$CXX -DWITH_EIGEN:BOOLEAN=TRUE ..
 fi
 
