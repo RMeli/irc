a7b965c3568a0d44f92dbacfe2d903270d3a3f9e
Rocco Meli <r.meli@bluemail.ch>
2018-01-26 13:07:01 +0000

    Linear algebra choice
    TODO: Add default

--- .travis/build.sh
+++ .travis/build.sh
@@ -4,7 +4,11 @@ mkdir -p build
 
 cd build
 
-cmake -DCMAKE_C_COMPILER=$CC -DCMAKE_CXX_COMPILER=$CXX ..
+if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+    cmake -DCMAKE_C_COMPILER=$CC -DCMAKE_CXX_COMPILER=$CXX -DWITH_ARMA:BOOLEAN=TRUE ..
+elif [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
+    #cmake -DCMAKE_C_COMPILER=$CC -DCMAKE_CXX_COMPILER=$CXX -DWITH_EIGEN:BOOLEAN=TRUE ..
+fi
 
 make -j
 
