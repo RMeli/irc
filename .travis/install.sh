7bb2768cae59bc92887dc0df6a5e4b4d5cae9fc0
Rocco Meli <r.meli@bluemail.ch>
2018-01-29 13:18:48 +0000

    Spelling error
    Output
    
    
    Export

--- .travis/install.sh
+++ .travis/install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+    echo "Installing ${LIBLA}..."
     if [[ "$LIBLA" == "arma" ]]; then
         brew install armadillo
     elif [[ "$LIBLA" == "eigen" ]]; then
