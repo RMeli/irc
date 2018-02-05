371359345afb8b887bc914526a2f9098d15ff56a
Rocco Meli <r.meli@bluemail.ch>
2018-02-05 17:39:20 +0000

    Removed Boost and optional coverage

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -3,5 +3,5 @@
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
     rm /usr/local/include/c++
     brew update
-    # brew outdated boost || brew upgrade boost
+    brew outdated boost || brew upgrade boost
 fi
