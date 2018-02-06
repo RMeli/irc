fb20570de5b8fc935c508d7b4d16bbaf990c903a
Rocco Meli <r.meli@bluemail.ch>
2018-02-06 12:34:07 +0000

    Print functions

--- include/libirc/io.h
+++ include/libirc/io.h
@@ -2,9 +2,12 @@
 #define IRC_IO_H
 
 #include "molecule.h"
+#include "connectivity.h"
 
 #include <fstream>
+#include <iomanip>
 #include <stdexcept>
+#include <vector>
 
 namespace irc {
 
@@ -49,6 +52,158 @@ molecule::Molecule<Vector3> load_xyz(std::string fname) {
   }
 
   return load_xyz<Vector3>(in);
+}
+
+/// Print bonds
+template <typename Vector3, typename Vector>
+void print_bonds(const Vector& x_c,
+                 const std::vector<connectivity::Bond>& bonds,
+                 std::ostream& out = std::cout ){
+
+  out << "+ -------- +" << std::endl;
+  out << "| Bonds (\u212B)|" << std::endl;
+  out << "+ -------- +" << std::endl;
+
+  // Total number of bonds
+  size_t n_bonds{bonds.size()};
+
+  // Atomic positions
+  Vector3 p1{0.,0.,0.}, p2{0.,0.,0.};
+
+  // Indices
+  size_t idx_i{0}, idx_j{0};
+
+  out.precision(3);
+  out.fill(' ');
+
+  for(size_t i{0}; i < n_bonds; i++){
+    // Get indices
+    idx_i = bonds[i].i;
+    idx_j = bonds[i].j;
+
+    // Print indices
+    out.width(1);
+    out << std::left << '(';
+    out.width(4);
+    out << std::right << idx_i << ',';
+    out.width(4);
+    out << std::right << idx_j << ')';
+
+    // Get positions
+    p1 = {x_c(3 * idx_i), x_c(3 * idx_i + 1), x_c(3 * idx_i + 2)};
+    p2 = {x_c(3 * idx_j), x_c(3 * idx_j + 1), x_c(3 * idx_j + 2)};
+
+    // Print distance
+
+    out << std::setw(14) << std::fixed << " " << std::right
+        << connectivity::distance(p1, p2) * tools::conversion::bohr_to_angstrom
+        << std::endl;
+  }
+}
+
+template <typename Vector3, typename Vector>
+void print_angles(const Vector& x_c,
+                 const std::vector<connectivity::Angle>& angles,
+                 std::ostream& out = std::cout ){
+
+  out << "+ --------- +" << std::endl;
+  out << "| Angles (\u00B0)|" << std::endl;
+  out << "+ --------- +" << std::endl;
+
+  // Total number of angles
+  size_t n_angles{angles.size()};
+
+  // Atomic positions
+  Vector3 p1{0.,0.,0.}, p2{0.,0.,0.}, p3{0.,0.,0.};
+
+  // Indices
+  size_t idx_i{0}, idx_j{0}, idx_k{0};
+
+  out.precision(2);
+  out.fill(' ');
+
+  for(size_t i{0}; i < n_angles; i++){
+    // Get indices
+    idx_i = angles[i].i;
+    idx_j = angles[i].j;
+    idx_k = angles[i].k;
+
+    // Print indices
+    out.width(1);
+    out << std::left << '(';
+    out.width(4);
+    out << std::right << idx_i << ',';
+    out.width(4);
+    out << std::right << idx_j << ',';
+    out.width(4);
+    out << std::right << idx_k << ')';
+
+    // Get positions
+    p1 = {x_c(3 * idx_i), x_c(3 * idx_i + 1), x_c(3 * idx_i + 2)};
+    p2 = {x_c(3 * idx_j), x_c(3 * idx_j + 1), x_c(3 * idx_j + 2)};
+    p3 = {x_c(3 * idx_k), x_c(3 * idx_k + 1), x_c(3 * idx_k + 2)};
+
+    // Print distance
+    out << std::setw(14) << std::fixed << std::right
+        << connectivity::angle(p1, p2, p3) * tools::conversion::rad_to_deg
+        << std::endl;
+  }
+}
+
+template <typename Vector3, typename Vector>
+void print_dihedrals(const Vector& x_c,
+                    const std::vector<connectivity::Dihedral>& dihedrals,
+                    std::ostream& out = std::cout ){
+
+  out << "+ ------------ +" << std::endl;
+  out << "| Dihedrals (\u00B0)|" << std::endl;
+  out << "+ ------------ +" << std::endl;
+
+  // Total number of angles
+  size_t n_dihedrals{dihedrals.size()};
+
+  // Atomic positions
+  Vector3 p1{0.,0.,0.}, p2{0.,0.,0.}, p3{0.,0.,0.}, p4{0.,0.,0.};
+
+  // Indices
+  size_t idx_i{0}, idx_j{0}, idx_k{0}, idx_l{0};
+
+  out.precision(2);
+  out.fill(' ');
+
+  for(size_t i{0}; i < n_dihedrals; i++){
+    // Get indices
+    idx_i = dihedrals[i].i;
+    idx_j = dihedrals[i].j;
+    idx_k = dihedrals[i].k;
+    idx_l = dihedrals[i].l;
+
+    // Print indices
+    out.width(1);
+    out << std::left << '(';
+    out.width(4);
+    out << std::right << idx_i << ',';
+    out.width(4);
+    out << std::right << idx_j << ',';
+    out.width(4);
+    out << std::right << idx_k << ',';
+    out.width(4);
+    out << std::right << idx_l << ')';
+
+
+    // Get positions
+    p1 = {x_c(3 * idx_i), x_c(3 * idx_i + 1), x_c(3 * idx_i + 2)};
+    p2 = {x_c(3 * idx_j), x_c(3 * idx_j + 1), x_c(3 * idx_j + 2)};
+    p3 = {x_c(3 * idx_k), x_c(3 * idx_k + 1), x_c(3 * idx_k + 2)};
+    p4 = {x_c(3 * idx_l), x_c(3 * idx_l + 1), x_c(3 * idx_l + 2)};
+
+    // Print distance
+
+    out << std::setw(9) << std::fixed << std::right
+        << connectivity::dihedral(p1, p2, p3, p4)
+           * tools::conversion::rad_to_deg
+        << std::endl;
+  }
 }
 
 } // namespace io
--- src/test/io_test.cpp
+++ src/test/io_test.cpp
@@ -4,6 +4,7 @@
 
 #include "config.h"
 #include "libirc/molecule.h"
+#include "libirc/conversion.h"
 
 #include <iostream>
 #include <stdexcept>
@@ -11,9 +12,14 @@
 #ifdef HAVE_ARMA
 #include <armadillo>
 using arma::vec3;
+using arma::vec;
+using arma::mat;
 #elif HAVE_EIGEN3
 #include <Eigen3/Eigen/Dense>
 using vec3 = Eigen::Vector3d;
+using vec = Eigen::VectorXd;
+using mat = Eigen::MatrixXd;
+
 #else
 #error
 #endif
@@ -43,4 +49,46 @@ TEST_CASE("Loading XYZ file") {
   Molecule<vec3> mol{load_xyz<vec3>(config::molecules_dir + "caffeine.xyz")};
 
   std::cout << mol << std::endl;
+}
+
+TEST_CASE("Print"){
+  using namespace io;
+
+  using namespace connectivity;
+  using namespace molecule;
+  using namespace tools;
+
+  // Load toluene molecule
+  Molecule<vec3> mol{load_xyz<vec3>(config::molecules_dir + "toluene.xyz")};
+
+  // Transform molecular coordinates from angstrom to bohr
+  multiply_positions(mol, conversion::angstrom_to_bohr);
+
+  // Compute interatomic distance for formaldehyde molecule
+  mat dd{distances<vec3, mat>(mol)};
+
+  // Build graph based on the adjacency matrix
+  UGraph adj{adjacency_matrix(dd, mol)};
+
+  // Compute distance matrix and predecessor matrix
+  mat dist, predecessors;
+  std::tie(dist, predecessors) = distance_matrix<mat>(adj);
+
+  // Compute bonds
+  std::vector<Bond> B{bonds(dist, mol)};
+
+  // Print bonds to std::cout
+  print_bonds<vec3,vec>( to_cartesian<vec3,vec>(mol), B );
+
+  // Compute angles
+  std::vector<Angle> A{angles(dist, predecessors, mol)};
+
+  // Print angles to std::cout
+  print_angles<vec3,vec>( to_cartesian<vec3,vec>(mol), A );
+
+  // Compute dihedral angles
+  std::vector<Dihedral> D{dihedrals(dist, predecessors, mol)};
+
+  // Print dihedrals to std::cout
+  print_dihedrals<vec3,vec>( to_cartesian<vec3,vec>(mol), D );
 }
