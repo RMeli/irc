4f4f22d76502a8c1bd6234dbd16d0823a0bac13e
Peter Bygrave <bygrave@gmail.com>
2018-02-12 12:27:49 +0000

    Extend tests for periodic_table

--- src/test/periodic_table_test.cpp
+++ src/test/periodic_table_test.cpp
@@ -12,41 +12,30 @@ TEST_CASE("Test periodic table", "[periodic_table]") {
   using namespace irc;
   using namespace periodic_table;
 
-  bool verbose{false};
-
   // Check periodic table size for supported elements
-  SECTION("size") { REQUIRE(pt_size == 96); }
+  REQUIRE(pt_size == 96);
+  REQUIRE(pt_symbols.size() == pt_size);
+  REQUIRE(pt_covalent_radii.size() == pt_size);
+  REQUIRE(pt_vdv_radii.size() == pt_size);
+  REQUIRE(pt_masses.size() == pt_size);
 
   // Check valid atomic numbers
-  SECTION("atomic numbers") {
-    bool valid{false};
-    for (size_t i{0}; i < pt_size + 1; i++) {
-      valid = valid_atomic_number(i);
-
-      if (valid) {
-        REQUIRE(i > 0);
-        REQUIRE(i < pt_size);
-      } else {
-        REQUIRE((i == 0 or i == pt_size));
-      }
-    }
+  for (size_t atomic_num = 1; atomic_num < pt_size; atomic_num++) {
+    CAPTURE(atomic_num);
+    CHECK(valid_atomic_number(atomic_num));
   }
 
   // Check conversion from symbol to atomic number
-  SECTION("atomic number from symbol") {
-    for (size_t i{1}; i < pt_size; i++) {
-      REQUIRE(atomic_number(pt_symbols[i]) == i);
-    }
+  for (size_t atomic_num = 1; atomic_num < pt_size; atomic_num++) {
+    CAPTURE(atomic_num);
+    CAPTURE(pt_symbols[atomic_num]);
+    REQUIRE(atomic_number(pt_symbols[atomic_num]) == atomic_num);
   }
 
-  SECTION("invalid symbol") {
-    bool exception{false};
-    try {
-      atomic_number("ABC");
-    } catch (const std::logic_error &e) {
-      exception = true;
-    }
-
-    REQUIRE(exception == true);
-  }
+  CHECK_THROWS_AS(atomic_number("ABC"), std::logic_error);
+  CHECK_THROWS_AS(atomic_number(" H"), std::logic_error);
+  CHECK_THROWS_AS(atomic_number("H "), std::logic_error);
+  CHECK_THROWS_AS(atomic_number(" H "), std::logic_error);
+  CHECK_THROWS_AS(atomic_number("H,"), std::logic_error);
+  CHECK_THROWS_AS(atomic_number("H\n"), std::logic_error);
 }
