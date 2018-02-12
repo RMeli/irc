5a00b3ba5096522545bfda11e79ba4c46a4d22a6
Peter Bygrave <bygrave@gmail.com>
2018-02-12 12:10:31 +0000

    Extended testing for mathtools

--- src/test/mathtools_test.cpp
+++ src/test/mathtools_test.cpp
@@ -2,116 +2,70 @@
 
 #include "libirc/mathtools.h"
 
+#include <cmath> // nextafter
+
 using namespace irc;
 
-TEST_CASE("Angle in the interval ]-pi,pi]") {
+using tools::constants::pi;
+using namespace tools::math;
 
-  using tools::constants::pi;
-  using namespace tools::math;
+TEST_CASE("pirange_rad produces angles in the interval (-pi,pi]") {
 
-  SECTION("Angle in ]-pi,pi]") {
-    double angle{pi / 2.};
+  const auto inclusive_angle = pi;
+  CAPTURE(inclusive_angle);
 
-    Approx target{pi / 2.};
+  CHECK(pirange_rad(-pi) == Approx(inclusive_angle));
+  CHECK(pirange_rad(0) == 0);
+  CHECK(pirange_rad(pi) == Approx(inclusive_angle));
+  CHECK(std::nextafter(pi, 0) < pi);
+  CHECK(pirange_rad(std::nextafter(pi, 0)) == Approx(pi));
+  CHECK(pirange_rad(std::nextafter(pi, 2*pi)) == Approx(-pi));
+  CHECK(pirange_rad(std::nextafter(-pi, -2*pi)) == Approx(pi));
+  CHECK(pirange_rad(std::nextafter(-pi, 0)) == Approx(-pi));
 
-    target.margin(1e-6);
+  CHECK(pirange_rad(2*pi) == Approx(0));
+  CHECK(pirange_rad(4*pi) == Approx(0));
+  CHECK(pirange_rad(6*pi) == Approx(0));
+  CHECK(pirange_rad(8*pi) == Approx(0));
+  CHECK(pirange_rad(1*pi) == Approx(inclusive_angle));
+  CHECK(pirange_rad(3*pi) == Approx(inclusive_angle));
+  CHECK(pirange_rad(5*pi) == Approx(inclusive_angle));
+  CHECK(pirange_rad(-1*pi) == Approx(inclusive_angle));
+  CHECK(pirange_rad(-3*pi) == Approx(inclusive_angle));
+  CHECK(pirange_rad(-5*pi) == Approx(inclusive_angle));
 
-    REQUIRE(pirange_rad(angle) == target);
-  }
-
-  SECTION("Angle in ]pi,2*pi]") {
-    double angle{pi / 2. + pi};
-
-    Approx target{-pi / 2.};
-
-    target.margin(1e-6);
-
-    REQUIRE(pirange_rad(angle) == target);
-  }
-
-  SECTION("Angle in ]-2*pi,-pi]") {
-    double angle{-pi / 2. - pi};
-
-    Approx target{pi / 2.};
-
-    target.margin(1e-6);
-
-    REQUIRE(pirange_rad(angle) == target);
-  }
-
-  SECTION("Angle") {
-    double angle{pi / 2. + 6 * pi};
-
-    Approx target{pi / 2.};
-
-    target.margin(1e-6);
-
-    REQUIRE(pirange_rad(angle) == target);
-  }
-
-  SECTION("Angle") {
-    double angle{pi / 2. - 5 * pi};
-
-    Approx target{-pi / 2.};
-
-    target.margin(1e-6);
-
-    REQUIRE(pirange_rad(angle) == target);
-  }
+  CHECK(pirange_rad(pi/2.) == Approx(pi/2.));
+  CHECK(pirange_rad(-pi/2.) == Approx(-pi/2.));
+  CHECK(pirange_rad(pi/2. + pi) == Approx(-pi/2.));
+  CHECK(pirange_rad(-pi/2. - pi) == Approx(pi/2.));
 }
 
-TEST_CASE("Angle in the interval ]-180,180]") {
+TEST_CASE("pirange_deg produces angles in the interval (-180,180]") {
 
-  using tools::constants::pi;
-  using namespace tools::math;
+  const auto inclusive_angle = 180.0;
+  CAPTURE(inclusive_angle);
 
-  SECTION("Angle in ]-180,180]") {
-    double angle{90.};
+  CHECK(pirange_deg(-180) == Approx(inclusive_angle));
+  CHECK(pirange_deg(0) == 0);
+  CHECK(pirange_deg(180) == Approx(inclusive_angle));
+  CHECK(pirange_deg(std::nextafter(180, 0)) == Approx(180));
+  CHECK(pirange_deg(std::nextafter(180, 2*180)) == Approx(-180));
+  CHECK(pirange_deg(std::nextafter(-180, -2*180)) == Approx(180));
+  CHECK(pirange_deg(std::nextafter(-180, 0)) == Approx(-180));
 
-    Approx target{90.};
+  CHECK(pirange_deg(2*180) == Approx(0));
+  CHECK(pirange_deg(4*180) == Approx(0));
+  CHECK(pirange_deg(6*180) == Approx(0));
+  CHECK(pirange_deg(8*180) == Approx(0));
+  CHECK(pirange_deg(1*180) == Approx(inclusive_angle));
+  CHECK(pirange_deg(3*180) == Approx(inclusive_angle));
+  CHECK(pirange_deg(5*180) == Approx(inclusive_angle));
+  CHECK(pirange_deg(-1*180) == Approx(inclusive_angle));
+  CHECK(pirange_deg(-3*180) == Approx(inclusive_angle));
+  CHECK(pirange_deg(-5*180) == Approx(inclusive_angle));
 
-    target.margin(1e-6);
-
-    REQUIRE(pirange_deg(angle) == target);
-  }
-
-  SECTION("Angle in ]180,360]") {
-    double angle{270.};
-
-    Approx target{-90};
-
-    target.margin(1e-6);
-
-    REQUIRE(pirange_deg(angle) == target);
-  }
-
-  SECTION("Angle in ]-360,-180]") {
-    double angle{-270.};
-
-    Approx target{90.};
-
-    target.margin(1e-6);
-
-    REQUIRE(pirange_deg(angle) == target);
-  }
-
-  SECTION("Angle in ]360,540]") {
-    double angle{450.};
-
-    Approx target{90.};
-
-    target.margin(1e-6);
-
-    REQUIRE(pirange_deg(angle) == target);
-  }
-
-  SECTION("Angle in [-450,360[") {
-    double angle{-450};
-
-    Approx target{-90.};
-
-    target.margin(1e-6);
-
-    REQUIRE(pirange_deg(angle) == target);
-  }
+  CHECK(pirange_deg(90) == Approx(90));
+  CHECK(pirange_deg(-90) == Approx(-90));
+  CHECK(pirange_deg(270) == Approx(-90));
+  CHECK(pirange_deg(-270) == Approx(90));
 }
