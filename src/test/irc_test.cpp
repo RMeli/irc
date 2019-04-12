066222a66343104c5743ea0c576805a1123f8fa0
Rocco Meli <r.meli@bluemail.ch>
2019-04-12 21:34:42 +0100

    fix test
    switched molecule for irc test

--- src/test/irc_test.cpp
+++ src/test/irc_test.cpp
@@ -39,76 +39,128 @@ TEST_CASE("Internal Redundant Coordinates") {
 
   SECTION("User-defined Coordinates") {
 
-    // Define formaldehyde molecule (CH2O)
-    Molecule<vec3> molecule{{"C", {0.000000, 0.000000, -0.537500}},
-                            {"O", {0.000000, 0.000000, 0.662500}},
-                            {"H", {0.000000, 0.866025, -1.037500}},
-                            {"H", {0.000000, -0.866025, -1.037500}}};
+    // Define methanol molecule
+    Molecule<vec3> molecule{{"O", {0.0000000000,        0.0000000000,    0.0000000000}},
+                            {"H", {-0.9658081475,       0.0000000000,    0.0000000000}},
+                            {"C", {0.4371881141,        1.3664973705,    0.0000000000}},
+                            {"H", {0.0965595997,        1.9062256626,    0.8987823813}},
+                            {"H", {1.5337031672,        1.3396209523,    0.0000000000}},
+                            {"H", {0.0965596000,        1.9062256619,    -0.8987823831}}
+    };
 
     // Transform molecular coordinates from angstrom to bohr
     multiply_positions(molecule, angstrom_to_bohr);
 
     // Build internal reaction coordinates
-    // Add O-H bonds: (1,2)
-    // Add H-H bond: (2,3)
-    // Add H-O-H angle: (2,1,3)
-    // Add H-H-O-C dihedral: (3,2,1,0)
+    // Add H-H bonds: (3,4), (4,5), (5,3)
+    // Add H-O-H bonds: (1,0,3), (1,0,4), (1,0,5)
+    // Add H-H-H-H dihedral: (1,3,4,5)
+    // Add O-H-H-H dihedral: (0,3,4,5)
     IRC<vec3, vec, mat> irc(
-        molecule, {{1, 2}, {2, 3}}, {{2, 1, 3}}, {{3, 2, 1, 0}});
-
+        molecule, {{3, 4}, {4,5}, {5, 3}}, {{1, 0, 3}, {1, 0, 4}, {1, 0, 5}}, {{1, 3, 4, 5}, {0, 3,4, 5}});
+    
     // Compute internal coordinates
     vec q_irc{
         irc.cartesian_to_irc(molecule::to_cartesian<vec3, vec>(molecule))};
 
-    // Check size (3+2 bonds, 3+1 angles, 0+1 dihedrals)
-    REQUIRE(linalg::size(q_irc) == 10);
+    // Check size (5+3 bonds, 7+3 angles, 3+2 dihedrals)
+    REQUIRE(linalg::size(q_irc) == 23);
 
-    // Check manually added O-H bond
-    SECTION("Manually added O-H bond") {
-      Approx target(q_irc(3));
+    // Check manually added H-H bond
+    SECTION("Manually added H-H bond (1)") {
+      Approx target(q_irc(5));
       target.margin(1e-6);
 
       // Compute O-H distance
-      double d{distance(molecule[1].position, molecule[2].position)};
+      double d{distance(molecule[3].position, molecule[4].position)};
 
-      REQUIRE(d == target);
+      CHECK(d == target);
     }
 
     // Check manually added H-H bond
-    SECTION("Manually added H-H bond") {
-      Approx target(q_irc(4));
+    SECTION("Manually added H-H bond (2)") {
+      Approx target(q_irc(6));
       target.margin(1e-6);
 
-      // Compute H-H disance
-      double b{distance(molecule[2].position, molecule[3].position)};
+      // Compute O-H distance
+      double d{distance(molecule[4].position, molecule[5].position)};
 
-      REQUIRE(b == target);
+      CHECK(d == target);
     }
 
-    // Check manually added H-O-H angle
-    SECTION("Manually added H-O-H angle") {
-      Approx target(q_irc(8));
+    // Check manually added H-H bond
+    SECTION("Manually added H-H bond (3)") {
+      Approx target(q_irc(7));
+      target.margin(1e-6);
+
+      // Compute O-H distance
+      double d{distance(molecule[5].position, molecule[3].position)};
+
+      CHECK(d == target);
+    }
+    
+    // Check manually added H-O-H angle (1)
+    SECTION("Manually added H-O-H angle (1)") {
+      Approx target(q_irc(15));
       target.margin(1e-6);
 
       // Compute H-O-H angle
       double a{angle(
-          molecule[2].position, molecule[1].position, molecule[3].position)};
+          molecule[1].position, molecule[0].position, molecule[3].position)};
 
-      REQUIRE(a == target);
+      CHECK(a == target);
     }
 
-    // Check manually added H-H-O-C dihedral angle
-    SECTION("Manually added H-H-O-C dihedral angle") {
-      Approx target(q_irc(9));
+    // Check manually added H-O-H angle (2)
+    SECTION("Manually added H-O-H angle (2)") {
+      Approx target(q_irc(16));
       target.margin(1e-6);
 
-      // Compute H-H-O-C dihedral angle
-      double d{dihedral(molecule[3].position,
-                        molecule[2].position,
-                        molecule[1].position,
-                        molecule[0].position)};
+      // Compute H-O-H angle
+      double a{angle(
+        molecule[1].position, molecule[0].position, molecule[4].position)};
 
-      REQUIRE(d == target);
+      CHECK(a == target);
+    }
+
+    // Check manually added H-O-H angle (3)
+    SECTION("Manually added H-O-H angle (3)") {
+      Approx target(q_irc(17));
+      target.margin(1e-6);
+
+      // Compute H-O-H angle
+      double a{angle(
+        molecule[1].position, molecule[0].position, molecule[5].position)};
+
+      CHECK(a == target);
+    }
+
+    // Check manually added H-H-H-H dihedral angle
+    SECTION("Manually added H-H-H-H dihedral angle") {
+      Approx target(q_irc(21));
+      target.margin(1e-6);
+
+      // Compute H-O-C-H dihedral angle
+      double d{dihedral(molecule[1].position,
+                        molecule[3].position,
+                        molecule[4].position,
+                        molecule[5].position)};
+
+      CHECK(d == target);
+    }
+
+    // Check manually added O-H-H-H dihedral angle
+    SECTION("Manually added O-H-H-H dihedral angle") {
+      Approx target(q_irc(22));
+      target.margin(1e-6);
+
+      // Compute H-O-C-H dihedral angle
+      double d{dihedral(molecule[0].position,
+                  molecule[3].position,
+                  molecule[4].position,
+                  molecule[5].position)};
+
+      CHECK(d == target);
     }
   }
   /*
@@ -161,13 +213,16 @@ TEST_CASE("Internal Redundant Coordinates") {
   }
   */
 
-  SECTION("Initial hessian") {
+  SECTION("Hessian projection") {
 
-    // Define formaldehyde molecule (CH2O)
-    Molecule<vec3> molecule{{"C", {0.000000, 0.000000, -0.537500}},
-                            {"O", {0.000000, 0.000000, 0.662500}},
-                            {"H", {0.000000, 0.866025, -1.037500}},
-                            {"H", {0.000000, -0.866025, -1.037500}}};
+    // Define methanol molecule
+    Molecule<vec3> molecule{{"O", {0.0000000000,        0.0000000000,    0.0000000000}},
+                        {"H", {-0.9658081475,       0.0000000000,    0.0000000000}},
+                        {"C", {0.4371881141,        1.3664973705,    0.0000000000}},
+                        {"H", {0.0965595997,        1.9062256626,    0.8987823813}},
+                        {"H", {1.5337031672,        1.3396209523,    0.0000000000}},
+                        {"H", {0.0965596000,        1.9062256619,    -0.8987823831}}
+    };
 
     // Transform molecular coordinates from angstrom to bohr
     multiply_positions(molecule, angstrom_to_bohr);
@@ -191,7 +246,7 @@ TEST_CASE("Internal Redundant Coordinates") {
       Approx target(iH0(i));
       target.margin(1e-6);
 
-      REQUIRE(iH(i) == target);
+      CHECK(iH(i) == target);
     }
   }
 
